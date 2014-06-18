bdf9d48fa4b489bfae01874f048cea91ef12add2
agstwjhpoiky@gmail.com <agstwjhpoiky@gmail.com>
2014-06-17 20:11:24 -0400

    123

--- Binderfor446.h
+++ Binderfor446.h
@@ -1,8 +1,10 @@
 #include <string>
+using namespace std;
+
+
 typedef enum {MC, OTHER} Building;
 
 typedef struct room_location {
-<<<<<<< HEAD
     int room_ID;
     int charing_server_ID;
     string room_number;
@@ -10,28 +12,18 @@ typedef struct room_location {
     unsigned int room_in_charge;
     room_location(char* room_num, Building recv_building) {
     	string temp(room_num);
-=======
-    std::string room_number;
-    Building building;
-    room_location(char* room_num) {
-        std::string temp(room_num);
->>>>>>> 01220ba64ad518d7fab6f400275ca02416471c95
     	this->room_number = temp;
         this->building = recv_building;
     }
 } room_location;
 
 typedef struct client_info {
-<<<<<<< HEAD
     int ID;
     string host_name;
-=======
-    std::string host_name;
->>>>>>> 01220ba64ad518d7fab6f400275ca02416471c95
     unsigned short port;
     unsigned int num_room;
     client_info(char* IP) {
-        std::string temp(IP);
+    	string temp(IP);
     	this->host_name = temp;
         ID = 0;
     }
