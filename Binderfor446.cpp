41b9c19626932bc3ff3f8d850d986bc502d57051
agstwjhpoiky@gmail.com <agstwjhpoiky@gmail.com>
2014-06-18 16:52:19 -0400

    123

--- Binderfor446.cpp
+++ Binderfor446.cpp
@@ -280,7 +280,8 @@ int main(void)
     /* Print out server information */
     char hostname[256];
     gethostname(hostname,256);
-    cerr<< "BINDER_ADDRESS " << hostname <<endl;
+
+    cerr<< "BINDER_ADDRESS " << hostname << endl;
     cerr<< "BINDER_PORT " << ntohs(Server_addr.sin_port) <<endl;
     
 
